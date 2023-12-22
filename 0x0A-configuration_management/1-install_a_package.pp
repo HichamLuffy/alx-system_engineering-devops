62b1ba6fe5b652a67dc9aa1579ca49816c81646a
HichamLuffy <imalxvssteve2020@gmail.com>
2023-12-22 17:16:03 +0100

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # 1-install_a_package.pp
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.0.1',
   provider => 'pip3',
 }

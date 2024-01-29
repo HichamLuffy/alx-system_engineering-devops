fc575b1984486a4eaa1c2f0724a5cb46c60c00fe
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-29 23:13:24 +0100

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,7 +7,7 @@ import sys
 
 
 if __name__ == "__main__":
-    """ Gather data from an API"""
+    """Gather data from an API"""
     id_user = sys.argv[1]
     url = f'https://jsonplaceholder.typicode.com/'
     userid = f'users?id={id_user}'

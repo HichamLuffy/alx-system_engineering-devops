185e29619d4bc6d90604f38aba8066357ccf2a42
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-29 21:20:51 +0100

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#  Gather data from an API
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    """ Gather data from an API """
+    id_user = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + "users/{}".format(id_user)).json()
+    todo = requests.get(url + "todos?userId={}".format(id_user)).json()
+
+    completed = [task.get("title") for task in todo if task.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todo)))
+    [print("\t {}".format(task)) for task in completed]

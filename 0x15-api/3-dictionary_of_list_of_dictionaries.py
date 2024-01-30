982dd2e4c5837c54d04f36aa429519fc5d07f1f7
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-30 02:36:19 +0100

    fix task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Export to CSV """
-
+""" Export to JSON for all employees """
 
 import json
 import requests
@@ -8,31 +7,29 @@ import sys
 
 
 if __name__ == "__main__":
-    """ Gather data from an API"""
-    id_user = sys.argv[1]
-    url = f'https://jsonplaceholder.typicode.com/'
-    userid = f'users?id={id_user}'
-    user_response = requests.get(f'{url}{userid}')
-    user = user_response.json()
-    todos = f'todos?userId={id_user}'
-    done = f'{todos}&completed=true'
-    todo_response = requests.get(f'{url}{todos}')
-    todo = todo_response.json()
-    Name = user[0].get("name")
-    todosdone = requests.get(f'{url}{done}').json()
-    todos_completed = len(todosdone)
-    totaldone = len(todo)
-    userName = user[0].get("username")
+    url = 'https://jsonplaceholder.typicode.com/'
+    users_response = requests.get(url + 'users')
+    users = users_response.json()
 
-    with open(f"{id_user}.json", "w") as f:
-        data = {
-            id_user: [
-                {
-                    "task": task.get("title"),
-                    "completed": task.get("completed"),
-                    "username": userName,
-                }
-                for task in todos
-            ]
-        }
-        json.dump(data, f)
+    all_tasks = {}
+
+    for user in users:
+        user_id = str(user['id'])
+        username = user['username']
+
+        todos_response = requests.get(url + f'todos?userId={user_id}')
+        todos = todos_response.json()
+
+        tasks = []
+        for todo in todos:
+            task_info = {
+                "username": username,
+                "task": todo["title"],
+                "completed": todo["completed"]
+            }
+            tasks.append(task_info)
+
+        all_tasks[user_id] = tasks
+
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(all_tasks, json_file, indent=4)

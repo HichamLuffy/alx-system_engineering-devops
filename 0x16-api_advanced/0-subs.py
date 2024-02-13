ee052d3619b7cd4e0defe66e2f4b2cb806676087
HichamLuffy <imalexvssteve2020@gmail.com>
2024-02-13 17:08:07 +0100

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,10 +2,7 @@
 """0 subs"""
 import requests
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0\
-    Safari/537.36'}
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def number_of_subscribers(subreddit):

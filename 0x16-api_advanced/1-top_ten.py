74ea2fe06b35fd00709198da90c6b8ae852ccdc6
HichamLuffy <imalexvssteve2020@gmail.com>
2024-02-13 17:17:11 +0100

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Top ten"""
+import requests
+
+
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0\
+        Safari/537.36'}
+
+def top_ten(subreddit):
+    """ returns the top ten hot posts"""
+
+
+    if subreddit is None or type(subreddit) is not str:
+        print(None)
+        return
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
